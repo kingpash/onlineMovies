5c8f1c1c0949bd4f0acca8c7277b667b9d4e6f9c
Pablo <pablo.bauti09@gmail.com>
2019-11-16 12:04:15 -0800

    Final changes to BookNow and checkseat

--- MovieBookingApp/Startup.cs
+++ MovieBookingApp/Startup.cs
@@ -30,6 +30,8 @@ namespace MovieBookingApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
